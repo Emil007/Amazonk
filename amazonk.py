1790a210c04f1e7b0a0b11d19391b34f7003e47a
Emil007 <emanuel@liesinger.net>
2023-04-25 01:31:51 +0200

    minor text fixes, 60min cooldown

--- amazonk.py
+++ amazonk.py
@@ -32,19 +32,19 @@ def send_notification(product_name, url, user_key):
         if random.choice([True, False]):
             message = f"{product_name} versteckt sich hinter Tor {random.choice(gates)}"
         else:
-            chosen_envelope = random.choices(envelopes, weights=[1, 1, 1, 1, 0.1], k=1)[0]
+            chosen_envelope = random.choices(envelopes, weights=[1, 1, 1, 0.9, 0.1], k=1)[0]
             if chosen_envelope == 'ZONK!!!!!!!!!!!!':
                 message = f"{product_name} ist {chosen_envelope}"
             else:
-                message = f"{product_name} ist im {chosen_envelope} Umschlag"
-        
+                message = f"{product_name} steckt im {chosen_envelope} Umschlag"
+
         # Check if the message is different from the previous one
         if message != previous_choice:
             previous_choice = message
             break
 
     print(f"Sending notification: {message}: {url}")
-    client.send_message(f"{message}: {url}", title="AMAZONK!")
+    client.send_message(f"{message}: {url}", title="AMAZONK!", html=True, sound="cashregister")
 
 def main():
     base_url = "https://www.amazon.de/dp/"
