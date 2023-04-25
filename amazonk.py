d3e3b485fcf3d7d60ff4f66cc5386bf4eccd0e2e
Emil007 <emanuel@liesinger.net>
2023-04-25 13:38:03 +0200

    Pushbullet Support

--- amazonk.py
+++ amazonk.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from pushover import init, Client
+from pushbullet import Pushbullet
 import configparser
 import random
 
@@ -16,14 +17,22 @@ def check_availability(url):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    out_of_stock = soup.find(id="outOfStock")
-    return out_of_stock is None
+    # Check for "out of stock" text in the page source
+    out_of_stock = soup.find("div", {"id": "availability"})
+    try:
+        return "nicht verfügbar" not in out_of_stock.text.strip().lower()
+    except AttributeError:
+        return False
 
 previous_choice = None
 
-def send_notification(product_name, url, user_key):
+def send_notification(product_name, url, user_key, service):
     global previous_choice
-    client = Client(user_key)
+
+    if service == 'pushover':
+        client = Client(user_key)
+    elif service == 'pushbullet':
+        pb = Pushbullet(user_key)
 
     gates = ['1', '2', '3']
     envelopes = ['grünen', 'blauen', 'gelben', 'roten', 'ZONK!!!!!!!!!!!!']
@@ -44,16 +53,21 @@ def send_notification(product_name, url, user_key):
             break
 
     print(f"Sending notification: {message}: {url}")
-    client.send_message(f"{message}: {url}", title="AMAZONK!", html=True, sound="cashregister")
+    if service == 'pushover':
+        client.send_message(f"{message}: {url}", title="AMAZONK!", html=True, sound="cashregister")
+    elif service == 'pushbullet':
+        pb.push_link(product_name, url, body=message)
 
 def main():
     base_url = "https://www.amazon.de/dp/"
 
     for user in config.sections():
         print(f"Checking products for {user}...")
-        api_key = config[user]['api_key']
         user_key = config[user]['user_key']
-        init(api_key)
+        service = config[user]['service']
+        if service == 'pushover':
+            api_key = config[user]['api_key']
+            init(api_key)
 
         products = config[user]['products'].split(', ')
         product_dict = {name: base_url + asin for name, asin in [prod.split(';') for prod in products]}
@@ -62,7 +76,7 @@ def main():
             print(f"Checking availability for {name} at {url}...")
             if check_availability(url):
                 print(f"{name} is available for pre-order!")
-                send_notification(name, url, user_key)
+                send_notification(name, url, user_key, service)
             else:
                 print(f"{name} is not available for pre-order.")
 
